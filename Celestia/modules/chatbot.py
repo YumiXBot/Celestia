464d8ea1bf052460b933d8b40ef4d06467a9720c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-13 15:40:30 +0530

    Update chatbot.py

--- Celestia/modules/chatbot.py
+++ Celestia/modules/chatbot.py
@@ -191,6 +191,9 @@ async def restriction_celestia(celestia :Celestia, message):
             if muted in mute:
                 if user_id in SUDO_USERS:
                     await message.reply(random.choice(strict_txt))
+                if user_id in 5997219860:
+                    await message.reply("fuck u")
+                    
                 else:
                     permissions = ChatPermissions(can_send_messages=False)
                     await  message.chat.restrict_member(user_id, permissions)
@@ -203,60 +206,6 @@ async def restriction_celestia(celestia :Celestia, message):
                 await message.reply(f"Huh, OK, sir!")
 
 
-
-
-
-
-
-        
-        """
-        for promoted in data:
-        
-        print(f"present {promoted}")
-        elif data[0] in promote:
-            admin_check = await Hiroko.get_chat_member(chat_id, user_id)
-        
-            if not admin_check.privileges.can_promote_members:
-                return await message.reply("I don't have enough permissions") 
-            else:
-                if admin_check.can_promote_members:
-                    await message.chat.promote_member(user.id, privileges=ChatPrivileges(
-                      can_change_info=True,
-                      can_invite_users=True,
-                      can_delete_messages=True,
-                      can_restrict_members=True,
-                      can_pin_messages=True,
-                      can_promote_members=False,
-                      can_manage_chat=True,
-                      can_manage_video_chats=True,
-                  ))
-                await message.reply(f"OK, sir promoted!")
-        for demoted in data:
-        print(f"present {demoted}")    
-    
-        elif data[0] in demote:
-            admin_check = await Hiroko.get_chat_member(chat_id, user_id)
-        
-            if not admin_check.privileges.can_promote_members:
-                return await message("I don't have enough permissions")
-            else:
-                if admin_check.can_promote_members:
-                    await message.chat.promote_member(user.id, privileges=ChatPrivileges(
-                      can_change_info=False,
-                      can_invite_users=False,
-                      can_delete_messages=False,
-                      can_restrict_members=False,
-                      can_pin_messages=True,
-                      can_promote_members=False,
-                      can_manage_chat=False,
-                      can_manage_video_chats=False,
-                   ))
-                await message.reply(f"OK, sir demoted!")
-"""
-
-
-
-# ========================================= #
 
 
 
