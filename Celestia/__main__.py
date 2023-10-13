10e61065146cf95bf09772490b1fa2ce4161feef
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-13 14:25:20 +0530

    Update __main__.py

--- Celestia/__main__.py
+++ Celestia/__main__.py
@@ -1,8 +1,8 @@
 import asyncio
 import importlib
 from pyrogram import idle
-from Hiroko import Hiroko
-from Hiroko.modules import ALL_MODULES
+from Celestia import Celestia
+from Celestia.modules import ALL_MODULES
 
  
 
@@ -11,7 +11,7 @@ loop = asyncio.get_event_loop()
 
 async def sumit_boot():
     for all_module in ALL_MODULES:
-        importlib.import_module("Hiroko.modules." + all_module)
+        importlib.import_module("Celestia.modules." + all_module)
     print("»»»» ʜᴇʀᴏᴋᴏ ʀᴏʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
     await idle()
     print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
