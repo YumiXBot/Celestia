d28d5110ef668b0acb86d2cd22316e67ec6bf7bf
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-14 13:41:51 +0530

    Update cleaner.py

--- Celestia/modules/cleaner.py
+++ Celestia/modules/cleaner.py
@@ -1,5 +1,5 @@
 import os
-from Hiroko import Hiroko
+from Celestia import Celestia
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -9,7 +9,7 @@ downloads_directory = os.path.join("Hiroko", "Helper", "downloader", "downloads"
 raw_directory = os.path.join("Hiroko", "Helper", "downloader", "raw_files")
 
 
-@Hiroko.on_message(filters.command(["rmd", "clear"], prefixes=["/", "!"]))
+@Celestia.on_message(filters.command(["rmd", "clear"], prefixes=["/", "!"]))
 async def clear_downloads(_, message: Message):
     ls_dir = os.listdir(downloads_directory)
     if ls_dir:
@@ -20,7 +20,7 @@ async def clear_downloads(_, message: Message):
         await message.reply_text("❌ **ɴᴏ ғɪʟᴇs ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ**")
 
 
-@Hiroko.on_message(filters.command(["rmw", "clean"], prefixes=["/", "!"]))
+@Celestia.on_message(filters.command(["rmw", "clean"], prefixes=["/", "!"]))
 async def clear_raw(_, message: Message):
     ls_dir = os.listdir(raw_directory)
     if ls_dir:
