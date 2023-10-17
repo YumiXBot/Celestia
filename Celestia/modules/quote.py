80a8e6d39bc775a743ea45e25048751ede50f4e4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-17 21:38:28 +0530

    Update quote.py

--- Celestia/modules/quote.py
+++ Celestia/modules/quote.py
@@ -2,9 +2,19 @@ from io import BytesIO
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from Celestia import Celestia
-from Celestia.Helper.http import fetch
+from httpx import AsyncClient, Timeout
 
 
+
+fetch = AsyncClient(
+    http2=True,
+    verify=False,
+    headers={
+        "Accept-Language": "id-ID",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42",
+    },
+    timeout=Timeout(20),
+)
 
 
 class QuotlyException(Exception):
