6504d50b38dec3c4c3147f39f7cdc574eff6bc08
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-22 18:53:58 +0530

    Update statuschk.py

--- Celestia/modules/statuschk.py
+++ Celestia/modules/statuschk.py
@@ -1,65 +1,55 @@
-import os
-import re
-import pytz
-import asyncio
-import datetime
-import config
-from pyrogram import Client, filters
+import os, re, pytz, asyncio, datetime
+from pyrogram import filters
 from pyrogram.errors import FloodWait
-from config import GROUP_ID, CHANNEL_OR_GROUP_ID, MESSAGE_ID, CHANNEL_NAME, TIME_ZONE, BOT_LIST, BOT_ADMIN_IDS
+from config import SUDO_USERS 
+from Celestia import Celestia, userbot
 
-
-
-
-
-app = Client(
-    name = "sumit",
-    api_id = config.API_ID,
-    api_hash = config.API_HASH,
-    session_string = config.SESSION_STRING
-)
+BOT_LIST = []
+MESSAGE_ID = []
+GROUP_ID = []
+CHANNEL_ID = []
 
 
 async def main_status():
-    async with app:
+    async with userbot:
             while True:
                 print("ᴄʜᴇᴄᴋɪɴɢ...")
-                xxx_teletips = f"<u>**🏷 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ {CHANNEL_NAME} ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴄʜᴀɴɴᴇʟ**</u>\n\n 📈 | <u>**ʀᴇᴀʟ ᴛɪᴍᴇ ʙᴏᴛ's sᴛᴀᴛᴜs 🍂**</u>"
+                xxx_teletips = f"<u>**🏷 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ {message.chat.title} ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴄʜᴀɴɴᴇʟ**</u>\n\n 📈 | <u>**ʀᴇᴀʟ ᴛɪᴍᴇ ʙᴏᴛ's sᴛᴀᴛᴜs 🍂**</u>"
                 for bot in BOT_LIST:
                     await asyncio.sleep(7)
                     try:
-                        bot_info = await app.get_users(bot)
+                        bot_info = await userbot.get_users(bot)
                     except Exception:
                         bot_info = bot
 
                     try:
-                        yyy_teletips = await app.send_message(bot, "/start")
+                        yyy_teletips = await userbot.send_message(bot, "/start")
                         aaa = yyy_teletips.id
                         await asyncio.sleep(15)
-                        zzz_teletips = app.get_chat_history(bot, limit = 1)
+                        zzz_teletips = userbot.get_chat_history(bot, limit = 1)
                         async for ccc in zzz_teletips:
                             bbb = ccc.id
                         if aaa == bbb:
                             xxx_teletips += f"\n\n╭⎋ **[{bot_info.first_name}](tg://user?id={bot_info.id})**\n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**"
-                            for bot_admin_id in BOT_ADMIN_IDS:
+                            for bot_admin_id in SUDO_USERS:
                                 try:
-                                    await app.send_message(int(GROUP_ID), f"**ᴋʏᴀ ᴋᴀʀ ʀᴀʜᴀ ʜᴀɪ ʙʜᴀɪ, 😡\n[{bot_info.first_name}](tg://user?id={bot_info.id}) ʙᴀɴᴅ ᴘᴀᴅᴀ ʜᴀɪ ᴡᴏʜ ᴛᴏʜ ᴀᴄᴄʜᴀ ʜᴜᴀ ᴍᴀɪɴᴇ ᴅᴇᴋʜ ʟɪʏᴀ**")
+                                    await Celestia.send_message(int(GROUP_ID), f"**ᴋʏᴀ ᴋᴀʀ ʀᴀʜᴀ ʜᴀɪ ʙʜᴀɪ, 😡\n[{bot_info.first_name}](tg://user?id={bot_info.id}) ʙᴀɴᴅ ᴘᴀᴅᴀ ʜᴀɪ ᴡᴏʜ ᴛᴏʜ ᴀᴄᴄʜᴀ ʜᴜᴀ ᴍᴀɪɴᴇ ᴅᴇᴋʜ ʟɪʏᴀ**")
                                 except Exception:...
                             await app.read_chat_history(bot)
                         else:
                             xxx_teletips += f"\n\n╭⎋ **[{bot_info.first_name}](tg://user?id={bot_info.id})**\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**"
-                            await app.read_chat_history(bot)
+                            await userbot.read_chat_history(bot)
                     except FloodWait as e:
                         ttm = re.findall("\d{0,5}", str(e))
                         await asyncio.sleep(int(ttm))
                 time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
                 last_update = time.strftime(f"%d %b %Y at %I:%M %p")
                 xxx_teletips += f"\n\n✅ <u>ʟᴀsᴛ ᴄʜᴇᴄᴋᴇᴅ ᴏɴ:</u>\n**ᴅᴀᴛᴇ & ᴛɪᴍᴇ: {last_update}**\n**ᴛɪᴍᴇ ᴢᴏɴᴇ: ({TIME_ZONE})**\n\n<i><u>♻️ ʀᴇғʀᴇsʜᴇs ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴡɪᴛʜɪɴ 4 ʜᴏᴜʀꜱ.</u></i>\n\n<i>**๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜᴍɪᴛ ʏᴀᴅᴀᴠ ๏**</i>"
-                await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
+                await userbot.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
                 print(f"ʟᴀsᴛ ᴄʜᴇᴄᴋᴇᴅ ᴏɴ: {last_update}")                
                 await asyncio.sleep(14400)
                         
-app.run(main_status())
+userbot.run(main_status())
 
 
 
