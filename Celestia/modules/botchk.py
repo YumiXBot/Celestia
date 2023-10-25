61c5f479095e743fce3d0db5f844a216825ebb6e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-25 15:59:08 +0530

    Update botchk.py

--- Celestia/modules/botchk.py
+++ Celestia/modules/botchk.py
@@ -2,26 +2,27 @@ import asyncio
 from pyrogram import filters
 from Celestia import Celestia, userbot
 
-BOT_LIST = ["CelestiaXBot", "ZuliAiBot", "KAYAMATMUSICBOT"]  
-
+BOT_LIST = ["CelestiaXBot", "ZuliAiBot", "KAYAMATMUSICBOT"]
 
 @Celestia.on_message(filters.command("botschk"))
 async def bots_chk(celestia, message):
-    alive_bots = []
-    dead_bots = []
-
+    msg = await message.reply("Checking bot stats...")
+    response = ""
     for bot_username in BOT_LIST:
         try:
-            bot_info = await userbot.get_users(bot_username)
-            alive_bots.append(bot_info)
+            bot = await userbot.get_users(bot_username)
+            bot_id = bot.id
+            bot_info = await userbot.send_message(bot, "/start")
+            bot_check = await userbot.get_chat_history(bot, limit=1)
+            for bot_message in bot_check:
+                if bot_message.from_user.id == bot_id:
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **Status: Online ✨**\n"
+                else:
+                    response += f"╭⎋ {bot_username}\n╰⊚ **Status: Offline ❄**\n"
         except Exception:
-            dead_bots.append(bot_username)
-
-    alive_msg = "\n".join(f"╭⎋ [{bot.first_name}](tg://user?id={bot.id}) \n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**" for bot in alive_bots)
-    dead_msg = "\n".join(f"╭⎋ {bot_username} \n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**" for bot_username in dead_bots)
-
-    response = "Bots list with live:\n" + alive_msg if alive_bots else "All bots are dead.\n" + dead_msg
-    await message.reply_text(response)
+            response += f"╭⎋ {bot_username}\n╰⊚ **Status: Error ❌**\n"
+    
+    await msg.edit_text(response)
 
 
 
