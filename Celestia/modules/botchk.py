1193d2ccc025458ba4af02f11d14a4f57edd0281
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-25 17:21:19 +0530

    Update botchk.py

--- Celestia/modules/botchk.py
+++ Celestia/modules/botchk.py
@@ -9,20 +9,20 @@ BOT_LIST = ["CelestiaXBot", "ZuliAiBot", "KAYAMATMUSICBOT"]
 
 @Celestia.on_message(filters.command("botschk") & filters.user(SUDO_USERS))
 async def bots_chk(celestia, message):
-    msg = await message.reply("Checking bot stats...")
-    response = ""
+    msg = await message.reply_photo(photo="https://telegra.ph/file/48578068b7574bb25a529.jpg", caption"**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ɪʀ ᴅᴇᴀᴅ...**")
+    response = "**ʙᴏᴛs sᴛᴀᴛs ᴅᴇᴀᴅ ᴏʀ ᴀʟɪᴠᴇ ᴄʜᴇᴄᴋᴇʀ**\n"
     for bot_username in BOT_LIST:
         try:
             bot = await userbot.get_users(bot_username)
             bot_id = bot.id
-            bot_info = await userbot.send_message(bot, "/start")
-            async for bot_message in userbot.get_chat_history(bot, limit=1):
+            bot_info = await userbot.send_message(bot_id, "/start")
+            async for bot_message in userbot.get_chat_history(bot_id, limit=1):
                 if bot_message.from_user.id == bot_id:
-                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **Status: Online ✨**\n\n"
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
                 else:
-                    response += f"╭⎋ {bot_username}\n╰⊚ **Status: Offline ❄**\n\n"
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**\n\n"
         except Exception:
-            response += f"╭⎋ {bot_username}\n╰⊚ **Status: Error ❌**\n"
+            response += f"╭⎋ {bot_username}\n╰⊚ **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ❌**\n"
     
     await msg.edit_text(response)
 
