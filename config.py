8e155c106c6d4a357f463252440f8b74632cdbaa
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-26 14:03:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "5997219860:AAGstFLBkn1NZ6GlgTLulOdsfPxplIzPQQs"
 OWNER_ID = int(getenv("OWNER_ID", "6280048819"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280048819 6691393517 5465943450").split()))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Hiroko:Hiroko@cluster0.1hztkgz.mongodb.net/?retryWrites=true&w=majority")
-SESSION_STRING = getenv("SESSION_STRING", "BQGZtJEASZs4Gx8ldmZ4usHpaOL_fzqvqVGdEV_GURyHGczzbvQSqAXyydlv-kt9O_C0cF9F1Gzh3MWLQNZ-ls1icSZKPexNlOryzXO9WyTYPwlUjt5_cUc3Z61xwuZ7jtJ5uVvW6nUdiF-LsE7a3-xuUmKdbRomeLbd-xDBOgx7fX7bt7oevzOIfmYXD1H-GX4-q990O346iPVwJNfZxF9yna19lsfqf_e2aLo38HrB7NywWYnzebhPIiVupCs67VMnGg8WnCbQXR4eHbxilIRiEL7mwywsyn5PGQtrNMvil8HrFor8dbrZQ-nJTAXeVVe1Pkox0wKhjq5g9ik8cRZvz9rffAAAAAFsCkpbAA")
+SESSION_STRING = getenv("SESSION_STRING", "BQGZtJEAKQesQBKmk9aQfF8WXN8VehRCqn2OpSEKIkstvfYGENWXjvjOojBUKbY5h1aEXU0XYeyNb9osCNfVh1M2vX1qb1XS9t98-GZqFCtYyZce-eVathwGRnxGdcqyguGCa8JJJ4OVDTbQUsfaXb8OTtSNpcbC5Rg3hh7JxzumE47t-Yivz2uO95xgpBZ15hBibhljN8rOxRiYIrAScPdQJxulHgRAvC-CvZkQaIofgxW9uPIqg090bHVWm-pGkg1FEc5pdQ6t8SqBhFQUpjsgx_6eD0SNaQ-md6RB_hSopl8jGYFSSpWvClAB2gT0HubueNP5dTluvxpaWLXmH6euEwiMpQAAAAFsCkpbAA")
