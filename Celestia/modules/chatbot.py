d4c5f06145b96ac95e149539d1742d84bc79ce7a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-27 18:27:49 +0530

    Update chatbot.py

--- Celestia/modules/chatbot.py
+++ Celestia/modules/chatbot.py
@@ -12,6 +12,15 @@ from Celestia.Helper.cust_p_filters import admin_filter
 
 
 
+
+def get_arg(message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
 # ========================================= #
 
 
