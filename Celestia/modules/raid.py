241d58cc85edce8fea05f620bac218c63885af5b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-27 17:06:30 +0530

    Update raid.py

--- Celestia/modules/raid.py
+++ Celestia/modules/raid.py
@@ -1,17 +1,10 @@
-import random, asyncio, time
-from typing import Tuple
-from traceback import format_exc
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import Message
-from pyrogram.errors import FloodWait
+import random
 from pyrogram import filters
 from Celestia import Celestia
 from config import SUDO_USERS, OWNER_ID
 from Celestia.Helper.database.raiddb import GROUPS, RAID
 
-
 ACTIVATE_LIST = []
-
 
 
 def get_arg(message):
@@ -23,51 +16,48 @@ def get_arg(message):
     return " ".join(split[1:])
 
 
-
-
-
 @Celestia.on_message(filters.user(SUDO_USERS) & filters.command(["replyraid", "rraid"], [".", "!"]))
-async def gban(celestia, message):
+async def replyraid(celestia, message):
     celu = await message.reply_text("**Processing**")
     reply = message.reply_to_message
     if reply:
-        user = reply.from_user["id"]
+        user = reply.from_user.id
     else:
         user = get_arg(message)
         if not user:
-            await celu.edit("**Whome should I replyraid?**")
+            await celu.edit("**Whom should I replyraid?**")
             return
     get_user = await celestia.get_users(user)
-    if int(message.chat.id) in GROUP:
-        await sex.edit("`abe bsdk sale mere group me mujhse hi spam krwayega !`")
+    if int(message.chat.id) in GROUPS:
+        await celu.edit("`Abe bsdk sale mere group me mujhse hi spam krwayega!`")
         return
     if int(get_user.id) in OWNER_ID:
-        await celu.edit("wew ye to mere pati dev hai..")
+        await celu.edit("Wew, ye to mere pati dev hai.")
         return
     elif int(get_user.id) in SUDO_USERS:
-        await celu.edit("Abe Lawde ye to mere dost hai .")
+        await celu.edit("Abe Lawde, ye to mere dost hai.")
         return
     elif int(get_user.id) in ACTIVATE_LIST:
-        await celu.edit("Already raid activate in this user.")
+        await celu.edit("Already raid activated for this user.")
         return
     ACTIVATE_LIST.append(get_user.id)
-    await celu.edit(f"**Successfully Reply Raid Started {get_user.first_name}!**")
-
+    await celu.edit(f"**Successfully Reply Raid Started for {get_user.first_name}!**")
 
 
 @Celestia.on_message(filters.user(SUDO_USERS) & filters.command(["dreplyraid", "drraid"], [".", "!"]))
-async def gbam(celestia, message):
+async def removeraid(celestia, message):
     reply = message.reply_to_message
     if reply:
-        user = reply.from_user["id"]
+        user = reply.from_user.id
     else:
         user = get_arg(message)
         if not user:
-            await message.reply_text("**Whome should I dreplyraid?**")
+            await message.reply_text("**Whom should I dreplyraid?**")
             return
     get_user = await celestia.get_users(user)
     ACTIVATE_LIST.remove(get_user.id)
-    await message.reply_text(f"**Reply Raid has Been Removed {get_user.first_name}, enjoy!**")
+    await message.reply_text(f"**Reply Raid has Been Removed for {get_user.first_name}, enjoy!**")
+
 
 
 @Celestia.on_message(filters.all)
@@ -77,17 +67,16 @@ async def check_and_del(celestia, message):
     if int(message.chat.id) in GROUPS:
         return
     try:
-        if not message.from_user.id in ACTIVATE_LIST:
+        if message.from_user.id not in ACTIVATE_LIST:
             return
     except AttributeError:
         return
-    message_id = message.message_id
     try:
         await message.reply_text(f"{random.choice(RAID)}")
-    except:
-        pass
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 
 
-  
+
