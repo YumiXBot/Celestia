e5dd127a7492dc0e93d451016c062ea8baa34868
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-29 17:45:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ BOT_USERNAME = getenv("BOT_USERNAME", "CelestiaXBot")
 COMMAND_HANDLER = ["/", "!"]
 BOT_TOKEN = getenv("BOT_TOKEN", "5997219860:AAGstFLBkn1NZ6GlgTLulOdsfPxplIzPQQs")
 OWNER_ID = int(getenv("OWNER_ID", "6280048819"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280048819").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280048819 6691393517").split()))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Hiroko:Hiroko@cluster0.1hztkgz.mongodb.net/?retryWrites=true&w=majority")
 SESSION_STRING = getenv("SESSION_STRING", "BQGZtJEAJFimS9sgw-I65ryfvAHm3q42WtWSiwhsEOs8dW080ngSBk0kOTtl9tCfpb1OU_0yU5Z2typ9qwLxafN7-R_r69uSUGlMx4f_GAGrBT03f0FeeCOYpiaMPF0iGCep9Oc3HAqMflosN8JpzwtEzN-eJHqAiIqw3hy6RF7BkG03lNBu4SO8-2uaq-vj0VEUn8ZjND017kmmgQKgpW2BDc_XTjLsUGPQCn28GKNuIPbBBIgPgqLnnSqi9niMoExQJ8bwsX-OYK0GmYkCmkni1aGrOS8xkLGzrSD233xPYe-YVlXYaI0KX1mxrlIp8hagWg0O9mkU0QCCmT7GXEMLjScTiAAAAAFsCkpbAA")
