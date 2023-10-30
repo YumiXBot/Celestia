e8b767579bbc7c6cb5a4d4a2d399bcef3b7475dd
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-30 11:49:20 +0530

    Update chatbot.py

--- Celestia/modules/chatbot.py
+++ Celestia/modules/chatbot.py
@@ -9,7 +9,7 @@ from lexica import Client
 from Celestia.Helper.database import *
 from pyrogram.enums import ChatMemberStatus, ChatType
 from Celestia.Helper.cust_p_filters import admin_filter
-#import g4f
+import g4f
 
 
 
