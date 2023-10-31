ac434a95652dea2930bd27b3958558b060702607
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-01 00:16:29 +0530

    Update pinterest.py

--- Celestia/modules/pinterest.py
+++ Celestia/modules/pinterest.py
@@ -91,6 +91,26 @@ async def playgrounai(_, message):
 
 
 
+@Celestia.on_message(filters.command(["reels"]))
+async def instagram_reel(client, message):
+    if len(message.command) == 2:
+        url = message.command[1]
+        response = requests.post(f"https://lexica-api.vercel.app/download/instagram?url={url}")
+        data = response.json()
+
+        if data['code'] == 2:
+            media_urls = data['content']['mediaUrls']
+            if media_urls:
+                video_url = media_urls[0]['url']
+                await message.reply_video(f"{video_url}")
+            else:
+                await message.reply("No video found in the response. may be accountbis private.")
+        else:
+            await message.reply("Request was not successful.")
+    else:
+        await message.reply("Please provide a valid Instagram URL using the /reels command.")
+
+
 
 
 
