7a22da2a7bb22ac8f09e7d6cf9704090bd27a446
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-31 12:28:55 +0530

    Create fight.py

---
+++ Celestia/modules/fight.py
@@ -0,0 +1,62 @@
+from pyrogram import Client, filters
+import random
+
+app = Client("your_bot")
+
+user_database = {}
+user_state = {}
+
+@Celestia.on_message(filters.command("character", prefixes="/"))
+def character_command(client, message):
+    user_id = message.from_user.id
+    if user_id not in user_state or user_state[user_id] != "character_created":
+        user_state[user_id] = "character_creation"
+        client.send_message(message.chat.id, "Create your character by using `/character {character name}`.")
+
+@Celestia.on_message(filters.create(lambda _, msg: msg.from_user.id in user_state and user_state[msg.from_user.id] == "character_creation"))
+def character_creation(client, message):
+    user_id = message.from_user.id
+    character_name = " ".join(message.command[1:])
+    if character_name:
+        user_database[user_id] = {"Name": character_name, "health": 100}
+        user_state[user_id] = "character_created"
+        client.send_message(message.chat.id, f"Character '{character_name}' created! You can now use the /fight command.")
+
+@Celestia.on_message(filters.command("fight", prefixes="/"))
+def fight_command(client, message):
+    user_id = message.from_user.id
+    if user_id not in user_database:
+        client.send_message(message.chat.id, "Please create your character first using the /character command.")
+        return
+
+    target_user_id = None
+    try:
+        target_user_id = int(message.command[1])
+    except (IndexError, ValueError):
+        client.send_message(message.chat.id, "Please specify a valid target user using `/fight user_id`.")
+        return
+
+    initiating_user = user_id
+
+    if target_user_id not in user_database:
+        client.send_message(message.chat.id, "Target user not found in the database.")
+        return
+
+    initiating_user_health = user_database[initiating_user]["health"]
+    target_user_health = user_database[target_user_id]["health"]
+
+    damage_initiator = random.randint(10, 30)
+    damage_target = random.randint(10, 30)
+
+    initiating_user_health -= damage_target
+    target_user_health -= damage_initiator
+
+    winner = initiating_user if initiating_user_health > target_user_health else target_user_id
+
+    result_message = f"{initiating_user} dealt {damage_initiator} damage. {target_user_id} dealt {damage_target} damage.\n"
+    result_message += f"{initiating_user} has {initiating_user_health} health. {target_user_id} has {target_user_health} health.\n"
+    result_message += f"The winner is {winner}!"
+
+    client.send_message(message.chat.id, result_message)
+
+app.run()
