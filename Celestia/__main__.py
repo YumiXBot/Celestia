af5413e2f7e232a303954cf232c29bcf2a1e6a56
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-01 16:57:13 +0530

    Update __main__.py

--- Celestia/__main__.py
+++ Celestia/__main__.py
@@ -3,6 +3,7 @@ import importlib
 from pyrogram import idle
 from Celestia import Celestia
 from Celestia.modules import ALL_MODULES
+from Celestia.modules.Games import GAMES_MODULES
 
  
 
@@ -13,6 +14,9 @@ async def sumit_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("Celestia.modules." + all_module)
      
+    for game_module in GAMES_MODULES:
+        importlib.import_module("Celestia.modules." + game_module)
+      
     print("»»»» ʜᴇʀᴏᴋᴏ ʀᴏʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
     await idle()
     print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
