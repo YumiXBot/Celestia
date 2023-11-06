94b66ad3c838383d5089b41209e0dacce935871f
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-06 17:48:38 +0530

    Update broadcast.py

--- Celestia/modules/broadcast.py
+++ Celestia/modules/broadcast.py
@@ -66,4 +66,38 @@ async def broadcast(_, message):
 
 
 
-                   
+
+
+@Celestia.on_message(filters.command("announce") & filters.user(OWNER_ID))
+async def announced(_, message):
+    if message.reply_to_message:
+      to_send=message.reply_to_message.id
+    if not message.reply_to_message:
+      return await message.reply_text("Reply To Some Post To Broadcast")
+    chats = await get_chats() or []
+    users = await get_users() or []
+    print(chats)
+    print(users)
+    failed = 0
+    for chat in chats:
+      try:
+        await Celestia.forward_messages(chat_id=int(chat), from_chat_id=message.chat.id, message_ids=to_send)
+        await asyncio.sleep(1)
+      except Exception:
+        failed += 1
+    
+    failed_user = 0
+    for user in users:
+      try:
+        await Celestia.forward_messages(chat_id=int(user), from_chat_id=message.chat.id, message_ids=to_send)
+        await asyncio.sleep(1)
+      except Exception as e:
+        failed_user += 1
+
+
+    await message.reply_text("Broadcast complete. {} groups failed to receive the message, probably due to being kicked. {} users failed to receive the message, probably due to being banned.".format(failed, failed_user))
+
+
+
+
+
