1b253c7246127e1a6307823691a5c40c9eeddf3c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-07 00:15:07 +0530

    Update musicbasss.py

--- Celestia/modules/musicbasss.py
+++ Celestia/modules/musicbasss.py
@@ -14,6 +14,7 @@ async def download_and_enhance_audio(client, message):
             audio = await reply_message.download()
             audio_segment = pydub.AudioSegment.from_file(audio)
             await celu.edit("now adding bass and uploading...")
+            
             enhanced_audio = audio_segment + 10           
             enhanced_audio.export("enhanced_audio.ogg", format="ogg")
             await celu.delete()
@@ -22,6 +23,32 @@ async def download_and_enhance_audio(client, message):
             await message.reply("The replied message is not an audio.")
     except Exception as e:
         await message.reply(f"An error occurred: {str(e)}")
+
+
+
+
+
+@Celestia.on_message(filters.command("loudly") & filters.reply)
+async def download_and_enhance_audio(client, message):
+    try:
+        reply_message = message.reply_to_message
+
+        if reply_message.audio:
+            celu = await message.reply("processing")
+            audio = await reply_message.download()
+            audio_segment = pydub.AudioSegment.from_file(audio)
+            await celu.edit("now adding loude audio and uploading...")
+        
+            louder_audio = audio_segment + 10
+            
+            louder_audio.export("louder_audio.ogg", format="ogg")
+            await celu.delete()
+            await message.reply_audio("louder_audio.ogg")
+        else:
+            await message.reply("The replied message is not an audio.")
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
+
 
 
 
