6d6eb1fb9960d4eab9cadc9014ed50f04b26d092
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-07 00:30:45 +0530

    Update musicbasss.py

--- Celestia/modules/musicbasss.py
+++ Celestia/modules/musicbasss.py
@@ -53,3 +53,20 @@ async def download_and_enhance_audio(client, message):
 
 
 
+@Celestia.on_message(filters.command("mono") & filters.reply)
+async def split_stereo_and_send_audio(client, message):
+    try:
+        reply_message = message.reply_to_message
+
+        if reply_message.audio:
+            a = pydub.AudioSegment.from_file(await reply_message.download())
+            b = a.split_to_mono()
+            mono_audio = b[0]
+            mono_audio.export("celestia.wav", format="wav")
+            await message.reply_audio("celestia.wav")
+        else:
+            await message.reply("The replied message is not an audio.")
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
+
+
