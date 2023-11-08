5326b3f52e43416cbacf98a413c28335fa571b86
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-09 00:08:05 +0530

    Update cleaner.py

--- Celestia/modules/cleaner.py
+++ Celestia/modules/cleaner.py
@@ -2,14 +2,14 @@ import os
 from Celestia import Celestia
 from pyrogram import filters
 from pyrogram.types import Message
+from config import SUDO_USERS
 
 
+downloads_directory = os.path.join("Celestia", "Helper", "downloader", "downloads")
+raw_directory = os.path.join("Celestia", "Helper", "downloader", "raw_files")
 
-downloads_directory = os.path.join("Hiroko", "Helper", "downloader", "downloads")
-raw_directory = os.path.join("Hiroko", "Helper", "downloader", "raw_files")
 
-
-@Celestia.on_message(filters.command(["rmd", "clear"], prefixes=["/", "!"]))
+@Celestia.on_message(filters.command(["rmd", "clear"], prefixes=["/", "!"]) & filters.user(SUDO_USERS))
 async def clear_downloads(_, message: Message):
     ls_dir = os.listdir(downloads_directory)
     if ls_dir:
@@ -20,7 +20,7 @@ async def clear_downloads(_, message: Message):
         await message.reply_text("❌ **ɴᴏ ғɪʟᴇs ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ**")
 
 
-@Celestia.on_message(filters.command(["rmw", "clean"], prefixes=["/", "!"]))
+@Celestia.on_message(filters.command(["rmw", "clean"], prefixes=["/", "!"]) & filters.user(SUDO_USERS))
 async def clear_raw(_, message: Message):
     ls_dir = os.listdir(raw_directory)
     if ls_dir:
