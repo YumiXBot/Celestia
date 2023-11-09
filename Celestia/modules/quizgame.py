034ef6fd3349e9756ea546d72f3bba36c4a2481a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-09 17:23:40 +0530

    Update quizgame.py

--- Celestia/modules/quizgame.py
+++ Celestia/modules/quizgame.py
@@ -167,30 +167,19 @@ async def callback_answer(client, query):
     user_id = query.from_user.id
     user_answer = query.data.replace('answer_', '')
 
-    if chat_id in DICT and 'correct_answer' in DICT[chat_id]:
+    if chat_id in DICT and DICT[chat_id].get("correct_answer"):
         correct_answer = DICT[chat_id]['correct_answer']
+        print(user_answer)
 
         if user_answer == correct_answer:
             DICT.pop(chat_id)
-            await query.answer("Your answer is correct!")
-            
-            # Check if the user has an account and create one if they don't
-            if not await is_player(user_id):
-                await create_account(user_id, query.from_user.username)
-                coins = await user_wallet(user_id)
-                x, y = await can_collect_coins(user_id)
-                
-                if x is True:
-                    # Update the user's coin balance
-                    await gamesdb.update_one({'user_id': user_id}, {'$set': {'coins': coins + 300}}, upsert=True)
+            await query.answer("your answer is correct!!")
+            await create_account(user_id,query.from_user.username)
+            coins = await user_wallet(user_id)     
+            await gamesdb.update_one({'user_id' : user_id},{'$set' : {'coins' : coins + 300}},upsert=True)
+            await edit_message_text("🎁 Yᴏᴜ ʜᴀᴠᴇ ᴄʟᴀɪᴍᴇᴅ ʏᴏᴜʀ ᴅᴀɪʟʏ ʙᴏɴᴜs ᴏғ ₤ 300 ᴅᴀʟᴄs!\n• ᴄᴜʀʀᴇɴᴛ ʙᴀʟᴀɴᴄᴇ ✑ ₤ `{0:,}`ᴅᴀʟᴄs".format(coins+300))    
                     
-                    # Edit the message to inform the user of their coin balance
-                    await query.edit_message_text("🎁 You have claimed your daily bonus of ₤ 300 Dalcs!\n• Current balance: ₤ `{0:,}` Dalcs".format(coins + 300))
-                
+            #await query.edit_message_text(f"{query.from_user.mention} **Your answer is correct! **")          
         else:
-            await query.answer("Your answer is wrong!")
-            await query.edit_message_text(f"{query.from_user.mention} Your answer is wrong!")
-
-
-
-
+            await query.answer("your answer is wrong!!")
+            await query.edit_message_text(f"{query.from_user.mention} **Your answer is wrong!**")
