278b7fd0e8b3fffc2a81bd19ef219a54f14d50d2
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-12 20:10:16 +0530

    Update profile.py

--- Celestia/modules/Games/profile.py
+++ Celestia/modules/Games/profile.py
@@ -56,48 +56,6 @@ def choose_character_callback(client, query):
 
 
 
-
-
-
-@Celestia.on_message(filters.command("profile", prefixes="/"))
-def profile_command(client, message):
-    user_id = message.from_user.id
-
-    if user_id not in user_database:
-        message.reply("You haven't created a character yet. Use the /character command to create one.")
-        return
-
-    character_data = user_database[user_id]
-    user_profile = f"""
-┏━━━━━━━━━━━━━━━━━
-┣ Umm Player profile 
-┗━━━━━━━━━━━━━━━━━
-┏━⦿
-┣⬢ Name : {character_data['name']}
-┣⬢ Health : {character_data['health']}
-┣⬢ Celeus : {character_data['celeus']}
-┣⬢ Player ID : {character_data['player_id']}
-┗━━━━━━━━━⦿
-
-┏━⦿
-┣ Exp : {character_data['experience']}
-┣ Level : {character_data['level']}
-┣ Rank : {character_data['rank']}
-┣ Location : {character_data['location']}
-┣ Battles Win : {character_data['battle_win']}
-┣ Total Battles : {character_data['total_win']}
-┗━━━━━━━━━⦿
-"""
-    reply_markup = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Family", callback_data="family_profile"),
-         InlineKeyboardButton("Shop", callback_data="open_shop")]
-    ])
-
-    message.reply_photo(photo="https://telegra.ph/file/55e27bacddf487d920a1a.jpg", caption=user_profile, reply_markup=reply_markup)
-
-
-
-
 @Celestia.on_message(filters.command("profile"))
 def profile_command(client, message):
     user_id = message.from_user.id
@@ -146,7 +104,7 @@ def profile_command(client, message):
 
 
 
-
+"""
 def get_arg(message):
     msg = message.text
     msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
@@ -751,3 +709,4 @@ def fight_command(client, message):
 
 
 
+"""
