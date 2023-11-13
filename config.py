93a983332332e19e03182bf0be281dd2d46bed78
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-13 09:59:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_USERNAME = getenv("BOT_USERNAME", "CelestiaXBot")
 COMMAND_HANDLER = ["/", "!"]
-BOT_TOKEN = getenv("BOT_TOKEN", "6711389550:AAHYtV97shWhg7UrtjTGWqDru6bH812aBtk")
+BOT_TOKEN = getenv("BOT_TOKEN", "6512407901:AAHIklvF7Z4-WlatvIbRMnEcsHrWJLNF-OI") # "6711389550:AAHYtV97shWhg7UrtjTGWqDru6bH812aBtk")
 OWNER_ID = int(getenv("OWNER_ID", "6280048819"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280048819 6691393517 737932898 6610172048 2072367717").split()))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Celestia:Celestia@celestia.08chke4.mongodb.net/?retryWrites=true&w=majority")
